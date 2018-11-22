83d8adc076fb6ab865406a88555ea684d1ca923b
salurs <orhsssalur@gmail.com>
2018-11-22 10:06:14 +0300

    filter method

--- src/components/list.js
+++ src/components/list.js
@@ -2,17 +2,34 @@ import React, { Component } from "react";
 import "./list.css";
 
 class List extends Component {
+  state = {
+    filterText: ""
+  };
+  onChangeFilterText = e => {
+    this.setState({
+      filterText: e.target.value
+    });
+  };
   render() {
+    const filterContacts = this.props.contacts.filter(contact => {
+      return (
+        contact.name
+          .toLowerCase()
+          .indexOf(this.state.filterText.toLowerCase()) !== -1
+      );
+    });
     return (
       <div className="listArea">
         <input
           type="text"
           name={"filter"}
           id={"filter"}
+          value={this.state.filterText}
+          onChange={this.onChangeFilterText}
           placeholder={"İsim yada numara ara"}
         />
         <ul className="list">
-          {this.props.contacts.map(contact => {
+          {filterContacts.map(contact => {
             return (
               <li key={contact.phone}>
                 <span className="name">{contact.name}</span>

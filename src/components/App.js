e1312c70f96204f6ea88572dec8d80b846ead81f
salurs <orhsssalur@gmail.com>
2018-11-22 09:04:39 +0300

    contact states 2

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,22 @@ import "../App.css";
 import Contacts from "./contacts";
 
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        name: "Orhan SALUR",
+        phone: "0777 777 77 77"
+      },
+      {
+        name: "Adnan ELTER",
+        phone: "0666 666 66 66"
+      }
+    ]
+  };
   render() {
     return (
       <div className="App">
-        <Contacts />
+        <Contacts contacts={this.state.contacts} />
       </div>
     );
   }
--- src/components/contacts.js
+++ src/components/contacts.js
@@ -3,22 +3,10 @@ import List from "./list";
 import Form from "./form";
 
 class Contacts extends Component {
-  state = {
-    contacts: [
-      {
-        name: "Orhan SALUR",
-        phone: "0777 777 77 77"
-      },
-      {
-        name: "Adnan ELTER",
-        phone: "0666 666 66 66"
-      }
-    ]
-  };
   render() {
     return (
       <div>
-        <List contacts={this.state.contacts} />
+        <List contacts={this.props.contacts} />
         <Form />
       </div>
     );
